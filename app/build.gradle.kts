fa11db64fe1ef623ae0a1124650ccf9db1b901d2
IslandofDream <rhffldkt0517@naver.com>
2024-03-04 04:44:13 +0900

    [Config] 라이브러리 사용을 위한 종속성추가 - 의존성 주입 : Hilt/Dagger - 서버통신 : Okhttp3, Retrofit, Kotlinx-serialization - 이미지 로드 : Coil - 서드파티 : Timber - Compose 라이브러리와의 호환성을 위해 jvm 17버전으로 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,20 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+    id("kotlinx-serialization")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "com.junwoo.elicemobliepa"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.junwoo.elicemobliepa"
         minSdk = 33
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,11 +34,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -66,4 +70,26 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.46.1")
+    kapt("com.google.dagger:hilt-compiler:2.46.1")
+
+    //okhttp
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation("com.squareup.okhttp3:logging-interceptor")
+    implementation("com.squareup.okhttp3:okhttp")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+
+    //coil
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    //kotlinx-serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
+    //timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
 }
