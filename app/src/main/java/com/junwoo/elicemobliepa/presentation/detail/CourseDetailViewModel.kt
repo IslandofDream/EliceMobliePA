bfc325db6c4573c5972a2b16daaf4b74b8685f6a
IslandofDream <rhffldkt0517@naver.com>
2024-03-05 15:17:12 +0900

    [Feat] Ui상태관리를 위한 SealedClass 추가 및 DetailCourseActivity 뷰모델에 적용 - StateFlow 및 UiState를 사용하여 반응형으로 동작하도록 설계 - 코루틴을 활용하여 비동기 서버통신 작업 수행

---
+++ app/src/main/java/com/junwoo/elicemobliepa/presentation/detail/CourseDetailViewModel.kt
@@ -0,0 +1,74 @@
+package com.junwoo.elicemobliepa.presentation.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.junwoo.elicemobliepa.core.ApiResult
+import com.junwoo.elicemobliepa.domain.entity.CourseDetailEntity
+import com.junwoo.elicemobliepa.domain.entity.LectureEntity
+import com.junwoo.elicemobliepa.domain.repository.DetailRepository
+import com.junwoo.elicemobliepa.presentation.util.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CourseDetailViewModel @Inject constructor(
+    private val detailRepository: DetailRepository
+) : ViewModel() {
+
+    private val _detailCourseStateFlow = MutableStateFlow<UiState<CourseDetailEntity>>(UiState.Init)
+    val detailCourseStateFlow = _detailCourseStateFlow.asStateFlow()
+
+    private val _lectureListStateFlow = MutableStateFlow<UiState<List<LectureEntity>>>(UiState.Init)
+    val lectureListStateFlow = _lectureListStateFlow.asStateFlow()
+
+    fun getCourseDetail(courseId: Int) {
+        viewModelScope.launch {
+            _detailCourseStateFlow.value = UiState.Loading
+            kotlin.runCatching {
+                detailRepository.getCourseDetail(courseId = courseId)
+            }.onSuccess {
+                it.collect { collect ->
+                    when (collect) {
+                        is ApiResult.Success -> {
+                            _detailCourseStateFlow.value = UiState.Success(collect.data)
+                        }
+
+                        is ApiResult.Error -> {
+                            _detailCourseStateFlow.value = UiState.Error(collect.message)
+                        }
+                    }
+                }
+            }.onFailure {
+                throw it
+            }
+        }
+    }
+
+    fun getLectureList(courseId: Int, offset: Int, count: Int) {
+        viewModelScope.launch {
+            _lectureListStateFlow.value = UiState.Loading
+            kotlin.runCatching {
+                detailRepository.getLectures(courseId = courseId, offset = offset, count = count)
+            }.onSuccess {
+                it.collect { collect ->
+                    when (collect) {
+                        is ApiResult.Success -> {
+                            _lectureListStateFlow.value = UiState.Success(collect.data)
+                        }
+
+                        is ApiResult.Error -> {
+                            _lectureListStateFlow.value = UiState.Error(collect.message)
+                        }
+                    }
+                }
+            }.onFailure {
+                throw it
+            }
+        }
+    }
+
+
+}
